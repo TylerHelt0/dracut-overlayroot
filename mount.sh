cee40d5b225eb2afb913fbcbcc9d7d195d8035af
tylerhelt0 <wesleyrobhelton@gmail.com>
2022-03-06 13:59:22 -0600

    Known good

--- mount.sh
+++ mount.sh
@@ -1,5 +1,3 @@
 	mkdir -p /run/overlayroot/{u,w,rootfs}
-	chmod -R 700 /run/overlayroot
-	mount --bind /sysroot /run/overlayroot/rootfs
 	mount -t overlay overlayroot -o lowerdir=/sysroot,upperdir=/run/overlayroot/u,workdir=/run/overlayroot/w /sysroot
 	return 0
