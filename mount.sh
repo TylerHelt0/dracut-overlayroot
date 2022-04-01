14f48623470ab9e978aa734443c83de621c8ec3b
TylerHelt0 <mendicantbastard@protonmail.com>
2022-03-31 20:52:50 -0500

    update mount.sh

--- mount.sh
+++ mount.sh
@@ -3,7 +3,7 @@ if ! getargbool 1 overlayroot -d -n rd_NO_OVERLAY; then
 	return 0
 else
 	info "Activating Overlayroot"
-	if mkdir -p /run/overlayroot/{u,w,rootfs,update}; then
+	if ! mkdir -p /run/overlayroot/{u,w,rootfs,update}; then
 		info "Overlayroot folder creation failed"
 		return 1
 	fi
@@ -14,11 +14,11 @@ else
 			info "Moving sysroot failed (2)"
 			return 1
 		fi
-		return 0
-		else
-			info "Moving sysroot failed (1)" 
-			return 1
+	else
+		info "Moving sysroot failed (1)" 
+		return 1
 	fi
+			
 
 	info "Mounting Overlayroot"
 	if mount -t overlay overlayroot -o lowerdir=/run/overlayroot/rootfs,upperdir=/run/overlayroot/u,workdir=/run/overlayroot/w /sysroot; then
