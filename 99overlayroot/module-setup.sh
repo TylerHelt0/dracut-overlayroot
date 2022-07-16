fb7bc9e7e38011d158d66321201b86c67d660540
TylerHelt0 <mendicantbastard@protonmail.com>
2022-07-16 15:04:47 -0500

    Add check logic, logic to install overlay kernel mod

--- 99overlayroot/module-setup.sh
+++ 99overlayroot/module-setup.sh
@@ -1,13 +1,19 @@
 #!/bin/ash
 
 check() {
+    require_binaries "chmod mount mkdir"
+    if [ ! $(cat /proc/filesystems|grep overlay|cut -f2) ]; then 
+        echo "Overlay filesystem support is not available on this kernel."
+        echo "Overlayroot not installed."
+        return 1
+    fi
     return 0
 }
 
-depends() {
-    return 0
+installkernel() {
+    instmods overlay
 }
 
 install () {
-    inst_hook pre-pivot 50 $moddir/mount.sh
-    }
+    inst_hook pre-pivot 50 $moddir/overlaymount.sh
+}
